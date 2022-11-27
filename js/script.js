7f9348e9973406c694f5dec3ea709a8e989d0711
Suryanshu Patnaik <suryanshupatnaik4@gmail.com>
2022-11-27 20:06:37 +0530

    main

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
+// --------------------DC SLIDER--------------------
 var dcsliderval;
 function dcsliderChange() {
   dcsliderval = document.getElementById("dc-slider").value;
   document.getElementById("dc-value").value = dcsliderval;
   check();
 }
-
+// --------------------RL SLIDER--------------------
 var rlsliderval;
 function rlsliderChange() {
   rlsliderval = document.getElementById("rl-slider").value;
   document.getElementById("rl-value").value = rlsliderval;
+  check();
+}
+// --------------------TEMP SLIDER--------------------
+var tempsliderval;
+function tpsliderChange() {
+  tempsliderval = document.getElementById("tp-slider").value;
+  document.getElementById("temp-value").value = tempsliderval;
   check();
 }
